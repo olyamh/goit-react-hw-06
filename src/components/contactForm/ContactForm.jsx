aa70e76e51681776e330c61b7d1927fd58bea082
Olyamh <olyamh234@yahoo.com>
2025-03-01 21:34:54 +0200

    direct commit

--- src/components/contactForm/ContactForm.jsx
+++ src/components/contactForm/ContactForm.jsx
@@ -5,7 +5,7 @@ import s from './ContactForm.module.css';
 import { useDispatch } from "react-redux";
 import { addContact } from "../../redux/ContactsSlice";
 
-const ContactForm = ({addContacts}) => {
+const ContactForm = () => {
   const initialValues = {
     name: "",
     number: "",
